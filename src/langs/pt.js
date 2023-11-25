3f15c7e5ed06449b3bc71ae47ac0c3c90613041f
Juan Pablo Franco Sanchez <juanpanime@gmail.com>
2023-11-25 03:08:46 -0500

    Improve traduction.

--- src/langs/pt.js
+++ src/langs/pt.js
@@ -1,6 +1,6 @@
 const PT = {
     title: "Eu sou Juan Pablo",
-    about: "Programador de software com mais de 6 anos de experiência em design, desenvolvimento e teste de aplicações web. Tenho experiência em programação em linguagens como Python, Javascript e Bash Scripting. Proficiente em administração de bases de dados MySQL, Oracle e MongoDB, bem como experiência em deployment e administração de aplicações em ambientes cloud Azure, Digital Ocean ou servidores linux."
+    about: "Programador de software com mais de 6 anos de experiência em design, desenvolvimento e teste de aplicações web. Tenho experiência em programação em linguagens como Python, Javascript e Bash Scripting. Proficiente em administração de bases de dados MySQL, Oracle e MongoDB, bem como experiência em implantação e administração de aplicações em ambientes cloud Azure, Digital Ocean ou servidores linux."
   };
   
 export default PT;
