e946824f3ef08ffebf1d600734b32b8892b10b03
Juan Pablo Franco Sanchez <juanpanime@gmail.com>
2023-11-25 02:57:27 -0500

    Better presentation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Text, Box, ButtonGroup, Grid, HStack } from '@chakra-ui/react'
+import { Box, Text,ButtonGroup, Grid, HStack} from '@chakra-ui/react'
 import ReactFlagsSelect from "react-flags-select";
 import { FaWhatsapp, FaLinkedin, FaGithub } from "react-icons/fa6";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import Img from "../public/original.png";
+import Reducedimg from "../public/reduced.jpg";
 import US from './langs/us';
 import ES from './langs/es';
 import PT from './langs/pt';
@@ -22,9 +24,9 @@ function App() {
   const currentTranslations = translations[language];
 
   return (
+    <>
       <Box p={0}>
-      <Grid templateColumns={{ base: '1fr', lg: '1fr 1fr' }} gap={4}>
-        <Box>
+      <Grid templateColumns={{ base: '1fr', lg: '1fr' }} gap={4}>
         <ButtonGroup gap='2'>
             <ReactFlagsSelect
             selected={language}
@@ -35,49 +37,38 @@ function App() {
             searchable={searchable}
             placeholder={language}
             />
-          </ButtonGroup>
-
-          <Box maxW="800px" mx="auto" px={{ base: 4, md: 6 }} py={{ base: 8, md: 12 }}>
-            <Text
-              bgGradient='linear(to-l, #000, #fff)'
-              bgClip='text'
-              fontSize={{ base: "4xl", md: "6xl" }}
-              fontWeight='extrabold'
-            >
-              {currentTranslations.title}
-            </Text>            
-            <Text fontSize={{ base: "lg", md: "xl" }} color="gray.600">
-              {currentTranslations.about}
-            </Text>
-          </Box>
-          <HStack spacing='24px' align='center' justifyContent='center'>
-            <a href="https://github.com/juanpfrancoS"><FaGithub fontSize='3em'/></a>
-            <a href="https://co.linkedin.com/in/juanpfrancos"><FaLinkedin fontSize='3em'/></a> 
-            <a href="https://api.whatsapp.com/send?phone=351925237446"><FaWhatsapp fontSize='3em'/></a>
-          </HStack>
-        </Box>
-
-        {/* Columna de imagen */}
-        <Box
-          display={{ base: 'none', lg: 'block' }}
-          gridColumn={{ base: '1', lg: '2' }}
-          style={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            height: '100%',
-          }}
-        >
-          <LazyLoadImage 
-          src="https://raw.githubusercontent.com/juanpfrancos/curriculum/master/original.png" 
-          alt="Me" 
-          style={{ width: '100%', margin: '0', padding: '0'}} 
-          placeholderSrc="https://raw.githubusercontent.com/juanpfrancos/curriculum/master/reduced.jpg"
-          effect="blur"
-          />
-        </Box>
+        </ButtonGroup>
       </Grid>
     </Box>
+    <Box
+      textAlign="center"
+      px={{ base: "5%", sm: "10%", md: "15%", lg: "20%" }}
+    >
+      <LazyLoadImage 
+          src={Img} 
+          alt="Me" 
+          style={{ width: '200px', margin: '0', padding: '0', borderRadius: '50%'}} 
+          placeholderSrc={Reducedimg}
+          effect="blur"
+          />
+      <Text
+          bgGradient='linear(to-l, #000, #fff)'
+          bgClip='text'
+          fontSize={{ base: "4xl", md: "6xl" }}
+          fontWeight='extrabold'
+        >
+          {currentTranslations.title}
+        </Text>            
+        <Text fontSize={{ base: "lg", md: "xl" }} color="gray.600">
+          {currentTranslations.about}
+        </Text>
+        <HStack spacing='24px' align='center' justifyContent='center'  style={{paddingBottom: '2em'}} >
+          <a href="https://github.com/juanpfrancoS"><FaGithub fontSize='3em'/></a>
+          <a href="https://co.linkedin.com/in/juanpfrancos"><FaLinkedin fontSize='3em'/></a> 
+          <a href="https://api.whatsapp.com/send?phone=351925237446"><FaWhatsapp fontSize='3em'/></a>
+        </HStack>
+    </Box>
+    </>
   );
 }
 
